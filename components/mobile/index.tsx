b0b8258dfa2a62fea2048af9778b6fe7b1604033
Jaehyeon99 <janhyun99@naver.com>
2022-11-14 19:35:48 +0900

    mobile 메인 작업

--- components/mobile/index.tsx
+++ components/mobile/index.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MobileHome: React.FC = () => {
+  const setting = {
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    fade: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    focusOnSelect: true,
+    arrows: false,
+    centerMode: true,
+    cssEase: "linear",
+  };
+
   return (
     <div className="flex flex-col">
       {/* Main Page - Main Animation 1  */}
@@ -33,22 +51,17 @@ const MobileHome: React.FC = () => {
       >
         <div className="h-[20%]" />
         <div className="flex flex-col justify-center items-center">
-          <div className="flex flex-col">
-            <div className="relative justify-center">
-              <Image
-                className="absolute"
-                src="/mobile/orange-star.png"
-                width={51}
-                height={50}
-              />
-              <Image
-                className="absolute"
-                src="/mobile/blue-star.png"
-                width={119}
-                height={144}
-              />
-            </div>
-            <span className="text-orange-500">WHAT WE DO</span>
+          <div className="flex justify-center">
+            <Image
+              src="/mobile/icon/What_We_do.png"
+              width={134}
+              height={143}
+              className="z-20"
+            />
+          </div>
+          <div className="flex flex-col space-y-4 text-white my-10">
+            <h2>온고지신</h2>
+            <h2>溫故知新</h2>
           </div>
           <div className="flex flex-col space-y-4">
             <span className="text-white w-full flex justify-center">
@@ -82,7 +95,7 @@ const MobileHome: React.FC = () => {
             각자의 방식으로 미래를 그리다
           </h2>
           <div className="h-[15vh] flex justify-center items-center">
-            <Image src="/mobile/orange-big-star.svg" width={50} height={50} />
+            <Image src="/mobile/icon/orange_logo.svg" width={50} height={50} />
           </div>
           <div className="flex flex-col space-y-4">
             <span className="text-white w-full flex justify-center">
@@ -102,7 +115,70 @@ const MobileHome: React.FC = () => {
             </span>
           </div>
         </div>
+        <div className="h-[35vh]" />
       </div>
+      <div className="bg-black h-[100vh]">
+        <div className="flex justify-center mt-[140px]">
+          <Image src="/mobile/icon/ourConcept.png" width={338} height={277} />
+        </div>
+        <div className="ml-24 mt-[30px]">
+          <div className="ml-4">
+            <Image src="/mobile/icon/orange_logo.svg" width={36} height={36} />
+          </div>
+          <div className="flex flex-col space-y-2 font-extralight">
+            <span className="text-white w-full flex justify-center">
+              무수한 과거와 현재가 이어져
+            </span>
+            <span className="text-white w-full flex justify-center">
+              미지의 공간인 미래를 만들어
+            </span>
+            <span className="text-white w-full flex justify-center">
+              간다는 의미를 담아 옛것을
+            </span>
+            <span className="text-white w-full flex justify-center">
+              흐리게, 새로운 것을 뚜렷하게
+            </span>
+            <span className="text-white w-full flex justify-center">
+              표현하여 과거와 미래가 공존
+            </span>
+            <span className="text-white w-full flex justify-center">
+              하는 온고지신을 그래픽으로
+            </span>
+            <span className="text-white w-full flex justify-center">
+              표현하였습니다.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-black">
+        <Slider {...setting}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index, res) => {
+            return (
+              <Image
+                key={index}
+                src="/mobile/icon/slide.png"
+                width={120}
+                height={18}
+              />
+            );
+          })}
+        </Slider>
+      </div>
+      <div className="bg-[#0649EC]">
+        <div className="flex justify-center mt-10 my-3">
+          <Image src={"/mobile/icon/gotoWork.png"} width={100} height={100} />
+        </div>
+        <div>
+          <h2 className="text-center text-[26px] text-white">
+            OUR
+            <br /> DEGREE SHOW
+          </h2>
+        </div>
+        <div className="text-white">
+          <h2>지도교수</h2>
+        </div>
+      </div>
+      <div className="h-[35vh]" />
     </div>
   );
 };
