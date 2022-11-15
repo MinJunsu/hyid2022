8775b83522fc28a01a9c5c0580fb4993fa916b72
Jaehyeon99 <janhyun99@naver.com>
2022-11-16 08:59:06 +0900

    잡혀서 하는중

--- components/desktop/index.tsx
+++ components/desktop/index.tsx
@@ -214,10 +214,14 @@ function Home({ viewCount }: HomeProps) {
                 </div>
 
                 <div className="flex-col text-white justify-center flex w-full text-center">
-                  <p className="text-[44px] font-normal">온고지신</p>
-                  <p className="text-[33px] font-extralight"> 溫故知新 </p>
-                  <p className="mt-[10vh] text-[22px] font-extralight">
-                    서당 개 3년이면 풍월을 읊는다고 하였습니다.
+                  <p className="text-4xl text-[#707070] font-medium">
+                    온고지신
+                  </p>
+                  <p className="text-[25px] font-extralight mt-2"> 溫故知新 </p>
+                  <p className="mt-[93px] text-lg font-light leading-9">
+                    <span className="font-medium">
+                      서당 개 3년이면 풍월을 읊는다고 하였습니다.
+                    </span>
                     <br /> 우리는 4년이라는 긴 시간 동안 교수님의 어깨 너머로
                     배운 것들을 스스로 깨닫고, <br />
                     산업 디자인과라는 일련의 과정 속에서 찾아낸 색으로 우리의
@@ -232,7 +236,7 @@ function Home({ viewCount }: HomeProps) {
             <div className="third bg-[url('/web/background/web_main_3_gif.gif')]  bg-cover bg-center h-[100vh] text-white  ">
               <Fade top cascade>
                 <div className="flex-col justify-center text-center items-center relative top-[30vh] ">
-                  <h2 className="text-[30px] mb-10">
+                  <h2 className="text-3xl mb-10">
                     각자의 방식으로 미래를 그리다
                   </h2>
                   <Image
@@ -241,14 +245,18 @@ function Home({ viewCount }: HomeProps) {
                     height={77}
                     alt="icon"
                   />
-                  <p className="text-[22px] mt-10 font-extralight">
-                    2022 한양대학교 산업디자인학과 졸업 전시 {"'"} 온고지신{" "}
+                  <p className="text-lg mt-10 font-light leading-9">
+                    2022 한양대학교 산업디자인학과 졸업 전시{" "}
+                    <span className="font-medium">{"'"} 온고지신 </span>
                     {"'"} 은
                     <br />
                     학생이라는 신분에서 배워 온 많은 것들을 몸으로 익히고,
                     <br />
-                    학교 밖을 벗어나 자신만의 새로운 미래를 만들어가는 전환점이
-                    될 것입니다.
+                    학교 밖을 벗어나{" "}
+                    <span className="font-medium">
+                      자신만의 새로운 미래를 만들어가는 전환점
+                    </span>
+                    이 될 것입니다.
                   </p>
                 </div>
               </Fade>
@@ -258,29 +266,37 @@ function Home({ viewCount }: HomeProps) {
           <div className="fourth">
             <div className=" bg-black  bg-cover bg-center h-auto text-white relative   ">
               <Fade top cascade>
-                <div className="pt-[490px] flex justify-center">
+                <div className="pt-[343px]  flex justify-center">
                   <Image
                     src="/web/background/concept.png"
-                    width={1450}
-                    height={900}
+                    width={1015}
+                    height={672}
                     alt="icon"
                   />
                 </div>
-                <div className="float-right mr-[80px]">
-                  <Image
-                    src="/web/icon/design_star.png"
-                    width={77}
-                    height={77}
-                    alt="icon"
-                  />
-                  <p className="font-extralight leading-7 ml-[50px]">
-                    무수한 과거와 현재가 이어져 미지의 공간인 미래를
-                    <br />
-                    만들어 간다는 의미를 담아 옛것을 흐리게, 새로운 것을
-                    <br /> 뚜렷하게 표현하여 과거와 미래가 공존하는 온고지신을
-                    <br />
-                    그래픽으로 표현하였습니다.
-                  </p>
+                <div className="relative float-right mr-48 -top-32">
+                  <div>
+                    <Image
+                      src="/web/icon/design_star.png"
+                      width={92}
+                      height={92}
+                      alt="icon"
+                    />
+                  </div>
+                  <div>
+                    <p className="font-light leading-7 ml-[50px]">
+                      무수한 과거와 현재가 이어져 미지의 공간인 미래를
+                      <br />
+                      만들어 간다는 의미를 담아
+                      <span className="font-medium">
+                        &nbsp;옛것을 흐리게, 새로운 것을
+                        <br /> 뚜렷하게 표현하여
+                      </span>{" "}
+                      과거와 미래가 공존하는 온고지신을
+                      <br />
+                      그래픽으로 표현하였습니다.
+                    </p>
+                  </div>
                 </div>
               </Fade>
             </div>
@@ -325,48 +341,48 @@ function Home({ viewCount }: HomeProps) {
               </div>
               <div className="madeBy mt-[125px]">
                 <div className="w-[1000px] m-auto pb-[250px]">
-                  <div className="grid grid-cols-6 space-x-[74px] mb-[40px]">
+                  <div className="grid grid-cols-6 space-x-[44px] mb-[40px]">
                     <div className="border-t-[1px]" />
                     <div className="border-t-[1px] w-[770px]" />
                   </div>
-                  <div className="grid grid-cols-6 space-x-[74px] ">
-                    <h1 className="text-[25px]">지도 교수</h1>
-                    <p className="text-[20px] font-extralight">고보형 교수</p>
-                    <p className="text-[20px] font-extralight">김태선 교수</p>
-                    <p className="text-[20px] font-extralight">박경진 교수</p>
-                    <p className="text-[20px] font-extralight">이재환 교수</p>
-                    <p className="text-[20px] font-extralight">한정완 교수</p>
+                  <div className="grid grid-cols-6 space-x-[64px] ">
+                    <h1 className="text-2xl font-bold">지도 교수</h1>
+                    <p className="text-[20px] font-normal">고보형 교수</p>
+                    <p className="text-[20px] font-normal">김태선 교수</p>
+                    <p className="text-[20px] font-normal">박경진 교수</p>
+                    <p className="text-[20px] font-normal">이재환 교수</p>
+                    <p className="text-[20px] font-normal">한정완 교수</p>
                   </div>
-                  <div className="grid grid-cols-6 space-x-[74px] pt-[60px]">
-                    <h1 className="text-[25px]">졸업준비위원회</h1>
-                    <p className="text-[20px] font-extralight">위원장</p>
-                    <p className="text-[20px] font-extralight">한윤정</p>
+                  <div className="grid grid-cols-6 space-x-[64px] pt-[60px]">
+                    <h1 className="text-2xl font-bold">졸업준비위원회</h1>
+                    <p className="text-[20px] font-medium">위원장</p>
+                    <p className="text-[20px] font-normal">한윤정</p>
                     <p></p>
                     <p></p>
                     <p></p>
                   </div>
-                  <div className="grid grid-cols-6 space-x-[74px]  pt-[55px]">
+                  <div className="grid grid-cols-6 space-x-[64px]  pt-[55px]">
                     <h1></h1>
-                    <p className="text-[20px] font-extralight">부위원장</p>
-                    <p className="text-[20px] font-extralight">임예지</p>
+                    <p className="text-[20px] font-medium">부위원장</p>
+                    <p className="text-[20px] font-normal">임예지</p>
                     <p className="text-[20px] font-extralight"></p>
                     <p></p>
                     <p></p>
                   </div>
-                  <div className="grid grid-cols-6 space-x-[74px]  pt-[55px]">
+                  <div className="grid grid-cols-6 space-x-[64px]  pt-[55px]">
                     <h1></h1>
-                    <p className="text-[20px] font-extralight">기획/홍보팀</p>
-                    <p className="text-[20px] font-extralight">이다은</p>
-                    <p className="text-[20px] font-extralight">공일빈</p>
+                    <p className="text-[20px] font-medium">기획/홍보팀</p>
+                    <p className="text-[20px] font-normal">이다은</p>
+                    <p className="text-[20px] font-normal">공일빈</p>
                     <p></p>
                     <p></p>
                   </div>
-                  <div className="grid grid-cols-6 space-x-[74px] pt-[55px] ">
+                  <div className="grid grid-cols-6 space-x-[64px] pt-[55px] ">
                     <h1></h1>
-                    <p className="text-[20px] font-extralight">그래픽팀</p>
-                    <p className="text-[20px] font-extralight">이다빈</p>
-                    <p className="text-[20px] font-extralight">이민주</p>
-                    <p className="text-[20px] font-extralight">조호현</p>
+                    <p className="text-[20px] font-medium">그래픽팀</p>
+                    <p className="text-[20px] font-normal">이다빈</p>
+                    <p className="text-[20px] font-normal">이민주</p>
+                    <p className="text-[20px] font-normal">조호현</p>
                     <p></p>
                   </div>
                 </div>
@@ -376,16 +392,17 @@ function Home({ viewCount }: HomeProps) {
 
           <div className="footer bg-[#0649EC] border-t-[1px] border-white h-auto px-[200px]">
             <div className="py-[150px] text-white">
-              <h2 className="text-[25px] mb-[10px] ">
+              <h2 className="font-2xl mb-[10px] ">
                 Ongozisin, Hanyang univ. All Rights Reserved
               </h2>
               <div className="flex items-center justify-between ">
-                <p className="text-[20px] font-extralight">
+                <p className="font-2xl font-extralight">
                   59-9, Hanyangdaehak 1-gil, Sangnok-gu, Ansan-si, Gyeonggi-do,
                   Republic of Korea
                 </p>
-                <p className="text-[20px] font-extralight">
-                  Today {viewCount?.todayViewCount} | Total{" "}
+                <p className="font-xl font-extralight">
+                  Today {viewCount?.todayViewCount}{" "}
+                  <span className="m-10">|</span> Total{" "}
                   {viewCount?.allViewCount}
                 </p>
               </div>
